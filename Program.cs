887b1692541c8163b109a57596b8c72c2d446088
misha-solntcev <misha.solntcev@gmail.com>
2022-12-01 20:41:48 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 7. Найти все целые числа из промежутка от 1 до 300, 
+    у которых ровно пять делителей. */
+
+namespace Lab_3_2_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 300; i > 0; i--)
+            {
+                int count = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                        count++;
+                }
+                if (count == 5)
+                    Console.WriteLine($"i = {i}, count={count}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
